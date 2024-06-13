5e0bb87386bce93a3ddd4292f9642a4e976c7dca
Sergey Smolov <smolov@ispras.ru>
2024-06-13 17:48:23 +0300

    ieee-1364-2005: 16.02.02: annotate negative, add missing SDF
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- ieee-1364-2005/test_16_02_02_3.v
+++ ieee-1364-2005/test_16_02_02_3.v
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 ISP RAS (http://www.ispras.ru)
+ * Copyright 2018-2024 ISP RAS (http://www.ispras.ru)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,9 @@ module test(clk, mode, data, ntfr);
   input clk, mode, data, ntfr;
 
   specify
+`ifdef NEGATIVE_TEST
     $setuphold (posedge clk &&&  mode, data, 1, 1, ntfr); // Not annotated
     $setuphold (negedge clk &&& !mode, data, 1, 1, ntfr); // Not annotated
+`endif
   endspecify
 endmodule
