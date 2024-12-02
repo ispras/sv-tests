fdb5e491858e9875eb049441c7c38a7379d38672
Sergey Smolov <smolov@ispras.ru>
2024-12-02 14:26:26 +0300

    ieee-1364-2005/test_07_01_06_4.v: add missing DFF impl
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- ieee-1364-2005/test_07_01_06_4.v
+++ ieee-1364-2005/test_07_01_06_4.v
@@ -20,13 +20,31 @@
 //       7.1.6 Primitive instance connection list
 //         This example demonstrates how a series of modules can be chained together.
 
+module DFF(q, dat, clk);
+
+  input clk;
+  input dat;
+  output q;
+  reg out;
+
+  always @(posedge clk)
+    out = dat;
+
+  assign q = out;
+
+  specify
+    $setup (dat, posedge clk, 10);
+  endspecify
+
+endmodule
+
 module dffn(q, d, clk);
 
   parameter bits = 1;
   input [bits-1:0] d;
   output [bits-1:0] q;
   input clk ;
-//  DFF dff[bits-1:0] (q, d, clk); // create a row of D flip-flops
+  DFF dff[bits-1:0] (q, d, clk); // create a row of D flip-flops
 endmodule
 
 module MxN_pipeline(in, out, clk);
