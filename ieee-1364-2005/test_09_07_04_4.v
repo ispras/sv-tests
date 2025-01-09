d9e78dbfd599eec4b24b4196b5428bc94af8fca0
Sergey Smolov <smolov@ispras.ru>
2025-01-09 16:57:08 +0300

    test_09_07_04_2.v += *_04_03.v, 04_4.v
    Signed-off-by: Sergey Smolov <smolov@ispras.ru>

--- ieee-1364-2005/test_09_07_04_4.v
+++ ieee-1364-2005/test_09_07_04_4.v
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 ISP RAS (http://www.ispras.ru)
+ * Copyright 2025 ISP RAS (http://www.ispras.ru)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,9 +23,9 @@
 module test(a, b, c, d, e);
 
   input a, b, c, d, e;
-  reg r, rega;
+  reg r, regc;
 
-  always @(a, b, c, d, e) begin
-    r = rega;
+  always @(a or b, c, d or e) begin
+    r = regc;
   end
 endmodule
