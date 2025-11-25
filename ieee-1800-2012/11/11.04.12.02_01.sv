1a88eaf6a632af8970a21c996e6eefb57ec6e902
Ilina E.S. <kkatli717@gmail.com>
2025-11-25 10:29:33 +0300

    ieee-1800-2012: add chapter 11 (part 11.03-11.04.12)

---
+++ ieee-1800-2012/11/11.04.12.02_01.sv
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 ISP RAS (http://www.ispras.ru)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// IEEE Std 1800-2012
+//   11. Operators and expressions
+//    11.4 Operator descriptions
+//     11.4.12 Concatenation operators
+//      11.4.12.2 String concatenation
+
+// ! TYPE: POSITIVE
+
+module top;
+
+  string hello = "hello";
+  string s1;
+  int n = 3;
+  string s2 = {n { "boo " }};
+
+  initial begin
+    s1 = { hello, " ", "world" };
+    $display( "%s\n", s1 );
+    s1 = { s1, " and goodbye" };
+    $display( "%s\n", s1 );
+    $display( "%s\n", s2 ); // displays 'boo boo boo '
+  end
+endmodule
